b4aac0695cea58b36707b1dfeb05a53292f56ec1
Rahuldutta387 <rahuldutta387@gmail.com>
2024-05-24 20:44:32 +0530

    initial changes for navbar

---
+++ portfolio-maker/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import "./Navbar.css";
+const Navbar = () => {
+  const navbarMenuItems = [
+    {
+      title: "Home",
+    },
+    {
+      title: "Feature",
+    },
+    {
+      title: "Projects",
+    },
+    {
+      title: "Resume",
+    },
+    {
+      title: "Testimonial",
+    },
+    {
+      title: "Contact",
+    },
+  ];
+  return (
+    <nav className="navbar">
+      <div>i</div>
+      <div className="menulink">
+        <ul className="mainList">
+          {navbarMenuItems.map((item) => (
+            <li>{item.title}</li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
